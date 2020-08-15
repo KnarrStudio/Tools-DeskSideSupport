2ecd77e0668142eb3bcee98bf1e77eebbe69897c
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-08-15 19:16:59 -0400

    Update

--- Scripts/Uninstall-Software.ps1
+++ Scripts/Uninstall-Software.ps1
@@ -1,10 +1,4 @@
-﻿<#
-
-This is not working now.  It looks as if something broke.  
-I am coming back to this after a few months and think that it could be cleaner.
-Suggest splitting into two scripts, one that uses command line only one uses out-grid as a choice.
-
-#>
+﻿#requires -Version 3.0 -Modules PackageManagement
 
 function Uninstall-Software  
 {
@@ -12,61 +6,35 @@ function Uninstall-Software
       .SYNOPSIS
       Uninstall unneeded or unwanted software
   #>
-
+  
   [CmdletBinding(SupportsShouldProcess)]
   param
   (
-    [Parameter(Mandatory,HelpMessage = 'Software DisplayName', Position = 0)]
-    [String]$SoftwareName
+    [Parameter(Mandatory = $false,HelpMessage = 'Software DisplayName', Position = 0)]
+    [String[]]$RemoveSoftware
   )
-
-  $HKLMPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall', 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
   
-  $SoftwareList = (Get-ChildItem -Path $HKLMPath   |
-    Get-ItemProperty  | 
-    Where-Object -FilterScript {
-      $_.DisplayName -match $SoftwareName
-    } | Select-Object -Property DisplayName, UninstallString)
-    
-    $SoftwareList = $SoftwareList | Out-GridView -PassThru -Title 'Verify Software to Remove'
-    
-
-  #$SoftwareList
-
-  $MSIExecCount = 0
-  $EXECount = 0
-  foreach ($app in $SoftwareList) 
+  if(-not $RemoveSoftware)
   {
-  if(($app.UninstallString) -match 'MSIEXEC'){
-  Write-Verbose -Message 'MSIEXEC'
-  $MSIExecCount = $MSIExecCount + 1
-  $MSIExecCount
+    $InstalledSoftware = Get-Package
+    $RemovalSoftware = ($InstalledSoftware |
+    Select-Object -Property Name |
+    Out-GridView -PassThru -Title 'Verify Software to Remove')
   }
-  elseif(($app.UninstallString) -match 'EXE'){
-  Write-Verbose -Message ('EXE {0}' -f $app.UninstallString)
-  $EXECount = $EXECount + 1
-  $EXECount
-  }
-  }
-
-    Write-Verbose -Message ('App - {0}' -f $app)
-    #$App.UninstallString
-    If ($app.UninstallString) 
+  Else
+  {
+    foreach($EachSoftware in $RemovalSoftware)
     {
-      Write-Verbose -Message ('App Uninstall - {0}' -f $app.UninstallString)
-
-      $uninst = ($app.UninstallString)
-      Write-Verbose -Message ('UninstallString - {0}'  -f  $uninst)
-
-      $GUID = ($uninst.split('{')).trim('}')[1]
-      Write-Verbose -Message ('App GUID - {0}' -f  $GUID)
-
-      $app = Get-WmiObject -Class Win32_Product -ComputerName $env:COMPUTERNAME| Where-Object{$_ -match $GUID}
-      #$app.Uninstall()
-      #Start-Process -FilePath 'msiexec.exe' -ArgumentList "/X $GUID /passive" -Wait
-
-      Write-Verbose -Message $uninst
+      $RemovalSoftware = Get-Package -Name $EachSoftware
     }
   }
 
-Uninstall-Software -SoftwareName 'Java 8 Update 161' -Verbose
+  
+  
+  foreach($EachSoftware in $RemovalSoftware)
+  {
+    Get-Package -Name $($EachSoftware.Name) |  Uninstall-Package -WhatIf
+  }
+}
+
+Uninstall-Software -RemoveSoftware 'Cisco eReader', 'Raspberry Pi Imager' 
